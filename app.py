d72df02c923583fd26f706991dcbff852e899f0f
Jonas Busch <92538801+buscjona@users.noreply.github.com>
2022-06-23 14:49:38 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from PIL import Image
+from img_classification import teachable_machine_classification
+
+st.title("Image Classification")
+st.header("Classification on examples")
+st.write("Upload a picture of a horse, dog, climber, football player, biker, child, or car. As these are the "
+         "categories, that the model was trained on. Trying pictures of other categories might result in not correct"
+         "captions!")
+
+uploaded_file = st.file_uploader("Upload a .jpg of your choice ...", type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded image.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    label = teachable_machine_classification(image, 'keras_model.h5')
+
+    for line in open('labels.txt'):
+        if str(label) in line:
+            st.write(line.split()[-1])
