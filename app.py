dcff26c4a24d71dd2cffba3e5a23e9fe685b13a0
Jonas Busch <92538801+buscjona@users.noreply.github.com>
2022-06-23 14:58:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 from PIL import Image
 from img_classification import teachable_machine_classification
 
-st.title("Image Classification")
-st.header("Classification on examples")
+st.title("Image Captioning")
+st.header("Captioning on examples")
 st.write("Upload a picture of a horse, dog, climber, football player, biker, child, or car. As these are the "
          "categories, that the model was trained on. Trying pictures of other categories might result in not correct"
-         "captions!")
+         " captions!")
 
 uploaded_file = st.file_uploader("Upload a .jpg of your choice ...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded image.', use_column_width=True)
     st.write("")
-    st.write("Classifying...")
+    st.write("Captioning...")
     label = teachable_machine_classification(image, 'keras_model.h5')
 
     for line in open('labels.txt'):
